609da11fb552aa0c438c5965b67dbe45e1548e2d
Gustaf Erikson <gerikson@gmail.com>
2025-11-20 11:35:48 +0100

    updated README

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Simple template for weekdays (Swedish locale)
 Inspired by <https://terokarvinen.com/2021/calendar-txt/>
 
 Proper ISO year/week handling per <http://johnbokma.com/blog/2019/09/04/iso-week-and-year-in-perl.html>
+
+Available online at <https://gerikson.com/cgi-bin/veckodagar.cgi>
--- generate.pl
+++ generate.pl
@@ -1,22 +1,40 @@
-#! /usr/bin/env perl
+#! /usr/bin/env perl -wT
 use Modern::Perl '2015';
 ###
 use utf8;
 binmode( STDOUT, ':encoding(UTF-8)' );
 
 use Time::Piece;
+use Data::Dump qw/dump/;
+
+use FindBin qw/$Bin/;
+BEGIN {
+    if ($Bin =~ m!([\w\./\-]+)!) {
+	$Bin = $1;
+    } else {
+	die "Bad directory $Bin";
+    }
+}
+use Template;
+use lib "$Bin/lib";
+use CGI qw(:standard start_ul *table -utf8);
+
+use CGI::Carp qw(fatalsToBrowser);
+use SwedishHolidays qw( swedish_months );
+
 
 # Swedish week day abbreviations
 Time::Piece::day_list(qw/sön mån tis ons tor fre lör/);
-
+#Time::Piece::mon_list(qw/Januari Februari Mars April Maj Juni Juli Augusti September Oktober November December/);
+#my @swe_mon = qw/januari februari mars april maj juni juli augusti september oktober november december/;
 
 # user input for year, otherwise current year
 my $t = localtime;
-my $year = shift // $t->year;
+my $year = $ENV{QUERY_STRING} // $t->year;
 die "enter a valid year (1900 and onwards)"
     unless ( $year =~ m/\d{4}/ and $year >= 1900 );
 
-my %data;
+my $year_weeks;
 
 for my $mon ( 1 .. 12 ) {
 
@@ -37,17 +55,37 @@ for my $mon ( 1 .. 12 ) {
 
         # we use ISO week year ("%G") for the hash key, see
         # http://johnbokma.com/blog/2019/09/04/iso-week-and-year-in-perl.html
-        push @{ $data{ sprintf( "%04dW%02d", $day->strftime("%G"), $day->week ) } },
-            join( ' ', $day->date, $day->day, ' ' );
+	my $YW = sprintf( "%04dW%02d", $day->strftime("%G"), $day->week );
+        push @{ $year_weeks->{ $YW }->{day} }, $day;
+	$year_weeks->{$YW}->{mon}->{$day->mon}++;
     }
 
 }
+
+my $data;
+# format for output
 
 # output the data
-for my $yw ( sort keys %data ) {
-    say "\n### $yw\n";
-    for my $d ( @{ $data{$yw} } ) {
-        say $d;
+push @{$data->{content}}, "# Veckokalender för $year\n";
+for my $yw ( sort keys %{$year_weeks} ) {
+    # for my $m (sort {$a<=>$b}keys %{$data{$yw}->{mon}}) {
+    # 	say "$m: ". $data{$yw}->{mon}{$m}
+    # }
+
+    if (scalar keys %{$year_weeks->{$yw}{mon}} > 1) {
+	push @{$data->{content}},"\n### $yw (".join(' - ', map {swedish_months($_)}sort {$a<=>$b} keys %{$year_weeks->{$yw}{mon}}) . ")\n";
+    } else {
+	push @{$data->{content}},  "\n### $yw (".swedish_months((keys %{$year_weeks->{$yw}{mon}})[0]).")\n";
     }
+    
+#    say "\n### $yw\n";
+     for my $d ( @{ $year_weeks->{$yw}{day}} ){
+         push @{$data->{content}}, sprintf("%s %s  ", $d->date, $d->day);
+     }
 }
 
+my $tt = Template->new( {INCLUDE_PATH=>"$Bin/templates"});
+my $out = header( {-type=>'text/markdown',-charset=>'utf-8'} );
+my $template= 'veckodagar.tt';
+$tt->process( $template, $data, \$out) or die $tt->error();
+print $out;
